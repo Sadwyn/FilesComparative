908bb6d1c8bab9ac0d09b7b64e464bb0d8ab6681
Kipris <pral-93@mail.ru>
2016-11-13 03:13:50 +0200

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,114 @@
+package sample;
+
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+
+public class Controller {
+    public ListView list;
+    private JFileChooser chooser = new JFileChooser();
+    ObservableList<File> array;
+
+
+    public void ChooseOnClick(ActionEvent actionEvent) {
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        int ref = chooser.showOpenDialog(null);
+
+        if (ref == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            File[] files = file.listFiles();
+            HashSet<File> arr = compareCheckSum(files);
+            array = new ObservableListWrapper<File>(new ArrayList<>(arr));
+            list.setItems(array);
+        }
+    }
+
+
+    public void DeleteOnClick(ActionEvent actionEvent) {
+        File file = (File) list.getSelectionModel().getSelectedItem();
+
+        if(list.getItems().size()>1){
+            file = (File) list.getSelectionModel().getSelectedItem();
+            file.delete();
+            array.remove(file);
+            list.setItems(array);
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Подтверждающее окно");
+            alert.setHeaderText("Удаление файла");
+            alert.setContentText("Вы действительно хотите удалить последний экземпляр файла?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.get()==ButtonType.OK){
+
+                file.delete();
+                array.remove(file);
+                list.setItems(array);
+            }
+        }
+
+    }
+
+    private HashSet<File> compareCheckSum(File[] files) {
+
+        HashSet<File> arrayList = new HashSet<>();
+        for (int i = 0; i < files.length; i++) {
+            for (int j = 0; j < files.length; j++) {
+                if(!(files[j].isDirectory()||files[i].isDirectory())) {
+                    if (i != j && getCheckSum(files[i]).equals(getCheckSum(files[j]))) {
+                        arrayList.add(files[j]);
+                        arrayList.add(files[i]);
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    private String getCheckSum(File file) {
+        String FILENAME = file.getAbsolutePath();
+        String ALGORITHM = "SHA-1";
+        StringBuilder sb = new StringBuilder();
+        try {
+            // Получаем контрольную сумму для файла в виде массива байт
+            final MessageDigest md = MessageDigest.getInstance(ALGORITHM);
+            final FileInputStream fis = new FileInputStream(FILENAME);
+            byte[] dataBytes = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(dataBytes)) > 0) {
+                md.update(dataBytes, 0, bytesRead);
+            }
+            byte[] mdBytes = md.digest();
+            fis.close();
+
+            // Переводим контрольную сумму в виде массива байт в
+            // шестнадцатеричное представление
+
+            for (int i = 0; i < mdBytes.length; i++) {
+                sb.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+
+        } catch (FileNotFoundException | NoSuchAlgorithmException ex) {
+
+        } catch (IOException ex) {
+        }
+        return sb.toString();
+    }
+
+}
